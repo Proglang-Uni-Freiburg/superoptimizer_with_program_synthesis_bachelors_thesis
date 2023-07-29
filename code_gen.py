f36e5db78c99ce51b6d2be9db4f388188cb81ec2
Lisa Hofert <hofertlisa@gmail.com>
2023-07-29 19:57:39 +0200

    added converter for input to python lambda

--- code_gen.py
+++ code_gen.py
@@ -1,10 +1,12 @@
 from ast import *
 from python_to_riscv import *
+from expr_to_func import *
 
 
 if __name__ == "__main__":
     in_expr = input("Please enter an arithmetic expression: ")
     in_expr_tree = parse(in_expr, mode='eval')
+    in_func = expr_to_func(in_expr_tree)  # convert input to python lambda with variables as args
     c = Compiler()
     match in_expr_tree:
         case Expression(body=to_analyse):
---
+++ expr_to_func.py
@@ -0,0 +1,23 @@
+from ast import *
+
+
+def expr_to_func(ast_in):
+    expr_vars = []
+    expr_body = Constant(value=0)
+    for node in walk(ast_in):
+        match node:
+            case Name(id, _) if id not in expr_vars:
+                expr_vars.append(id)
+            case Expression(body=b):  # for putting into lambda body later
+                expr_body = b
+            case _:
+                continue
+    
+    expr_vars.sort()
+    func = Expression(body=Lambda(args=arguments(posonlyargs=[], 
+                                                 args=[arg(arg=id) for id in expr_vars],
+                                                 kwonlyargs=[],
+                                                 kw_defaults=[],
+                                                 defaults=[]),
+                                  body=expr_body))
+    return eval(unparse(func))
