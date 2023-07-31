9f71398dddd84114220633714d49b3b8c9d492e1
Lisa Hofert <hofertlisa@gmail.com>
2023-07-31 22:46:09 +0200

    unfinished new idea for model checker

--- synthesize.py
+++ synthesize.py
@@ -7,10 +7,10 @@ from riscv_ast import *
 class Synthesizer:
     # goal_func: function
     # args: list  # list of function arguments
-
+    to_analyze = [ReturnReg()] + [Reg(x) for x in [5, 6, 7, 28, 29, 30, 31]]
     def __init__(self, f, args):
         self.goal_func = f
-        self.args = args
+        self.args = args  # TODO this needs to be converted from function args to proper args starting with a
     
     def verify(self, guess: list[Instr]):
         s = Solver()
@@ -22,19 +22,32 @@ class Synthesizer:
             # s.add(z3args[arg] < 256)
             # s.add(z3args[arg] > -256)
 
-        z3args["a0"] = Int("a0")
         Zero = Int("Zero")
         s.add(Zero == 0)
-            
-        for instr in guess:
-            match instr:
-                case Instr("add", (dest, reg1, reg2)):
-                    s.add(to_var(dest) == to_var(reg1) + to_var(reg2))
-                case Instr("addi", (dest, reg, imm)):
-                    s.add(to_var(dest) == to_var(reg) + imm)
-                case _:
-                    raise Exception("Not a valid RISC-V instruction")
+        z3args["a0"] = Int("a0")
+        s.add(z3args["a0"] == self.goal_func(*[z3args[x] for x in self.args]))
 
+        guess.reverse()
+        def match_instr(instrlist, goaldest):
+            # idea: go bottom up, use new variables for results. auflösen, sozusagen! inverse of python to riscv really.
+            # constants, zero and arguments stay unchanged. go thru temps, result reg
+            # perhaps change rule about arguments later on, as of now don't generate anything that overwrites arguments
+            for instr in instrlist:
+                match instr:
+                    case Instr("add" | "addi", (goaldest, arg1, arg2)):
+                        if arg1 in self.to_analyze:
+                            left = match_instr(guess[1:], arg1)
+                        else:
+                            left = to_var(arg1)
+                        if arg2 in self.to_analyze:
+                            right = match_instr(guess[1:],arg2)
+                        else:
+                            right = to_var(arg2)
+                        return left + right
+                    case _:
+                        raise Exception("Not a valid RISC-V instruction")
+
+        match_instr(guess, ReturnReg)
         if s.check() == sat:
             print("success!")
             print(s.model())
@@ -43,5 +56,5 @@ class Synthesizer:
 
 
 if __name__ == "__main__":
-    synth = Synthesizer(lambda x1: x1 + 1, ['x1'])
-    synth.verify([Instr("addi", Var(0), Reg(1), 1)])
+    synth = Synthesizer(lambda x1: x1 + 1, ['a2'])
+    synth.verify([Instr("addi", ReturnReg(), Var(2), 1)])
